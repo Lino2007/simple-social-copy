a07230ff016cf88b884db76b7b16799c3b51cc79
Lino Bevanda <44145624+Lino2007@users.noreply.github.com>
2022-12-18 23:16:32 +0100

    Add navbar and page footer (#25)
    * Add header and footer
    
    * Fix issue with collapsable navbar and align footer content

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
